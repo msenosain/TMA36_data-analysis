e139edf3e55b80e272593c04068e0cfeaf4d3120
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2021-09-14 17:17:12 -0500

    data cleaning script

---
+++ src/radiomics/10_datacleaning.R
@@ -0,0 +1,15 @@
+###############################################################################
+# Radiomics
+###############################################################################
+## 1. Healthmyne
+m_HM <- read.csv('data/TMA36_project/Radiomics/processed/TMA36_HealthMyne_Khushbu.csv')
+rownames(m_HM) <- m_HM[,1]
+m_HM <- m_HM[-which(is.na(m_HM[,2])),]
+m_HM <- m_HM[,15:ncol(m_HM)]
+nona <- colnames(m_HM)[apply(m_HM, 2, anyNA)]
+m_HM <- m_HM[,-which(colnames(m_HM) %in% nona)]
+
+# Center and scale
+m_HM <- scale(m_HM)
+
+write.csv(m_HM, 'data/TMA36_project/Radiomics/processed/rad_healthmyne.csv')
