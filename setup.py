928e6ca28dff342f2469c54f150bc0fa409e2fe9
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2021-10-13 21:36:13 -0500

    python environment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Capston project fe project from Team #120 as part of the DS4A/Empowerment program',
+    author='Team 120 (DS4A/Empowerment - Correlation One)',
+    license='MIT',
+)
