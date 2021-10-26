5f235839299ee7808c8aaed009799bd11513d219
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2021-10-26 16:17:10 -0500

    modifying mutational load calc

--- src/wes/10_WES_binarymatrix.R
+++ src/wes/10_WES_binarymatrix.R
@@ -12,7 +12,7 @@ x = getClinicalData(mut)
 x$Tumor_Sample_Barcode = as.character(x$Tumor_Sample_Barcode)
 pt_ID <- sapply(strsplit(x$Tumor_Sample_Barcode, "pt"), "[[", 2)
 pt_ID <- sapply(strsplit(pt_ID, "_"), "[[", 1)
-CDE <- read.csv('data/TMA36_project/CDE/CDE_TMA36_2020FEB25_SA_MF.csv')
+CDE <- read.csv('data/TMA36_project/CDE/CDE_TMA36_2021SEPT21_DR_MF.csv')
 
 CDE <- CDE[match(pt_ID, CDE$pt_ID),]
 CDE <- cbind('Tumor_Sample_Barcode'=x$Tumor_Sample_Barcode, CDE)
@@ -39,7 +39,12 @@ colnames(mut_dt) <- sapply(strsplit(colnames(mut_dt), "_"), "[[", 1)
 mut_dt <- t(mut_dt)
 
 # Compute mutational load (number of mutations per patient)
-mut_load = data.frame(mut_load = apply(mut_dt, 1, sum))
+#mut_load = data.frame(mut_load = apply(mut_dt, 1, sum))
+mut_load <- data.frame(table(mut_df$Tumor_Sample_Barcode))
+colnames(mut_load) <- c('pt_ID', 'mut_load')
+mut_load$pt_ID <- sapply(strsplit(as.character(mut_load$pt_ID), "pt"), "[[", 2)
+mut_load$pt_ID <- sapply(strsplit(as.character(mut_load$pt_ID), "_"), "[[", 1)
+
 
 # Remove genes that are mutated in less than X% of samples
 prcnt <- 0.1 # cutoff: genes must be mutated in >10 % of the samples
@@ -50,7 +55,8 @@ mut_dt_sum <- data.frame(t(mut_dt)) %>%
   dplyr::select(genes, sum) %>%
   filter(., sum > nrow(mut_dt)*prcnt)
 
-mut_dt <- mut_dt[,mut_dt_sum$genes]
-mut_dt <- cbind(mut_load, mut_dt)
+mut_dt <- data.frame(mut_dt[,mut_dt_sum$genes]) %>%
+  mutate(pt_ID=rownames(.))%>%
+  left_join(mut_load, ., by = 'pt_ID')
 
 write.csv(mut_dt, file = "data/TMA36_project/WES/processed/wes_binary.csv")
